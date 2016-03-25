9cbac808ec1606b0a2df066bbf58e35d129706ae
Dillon Wastrack <jdwastrack@crimson.ua.edu>
2016-03-25 12:25:30 -0500

    added new version

--- 5.38/5.38.c
+++ 5.38/5.38.c
@@ -9,33 +9,8 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 void *partC(void*);
-//void *partA(void*);
 void increase_count(int);
 void decrease_count(int);
-//int increase_count_monitor(int);
-//int decrease_count_monitor(int);
-
-//struct monitor
-//{
-//	int available_resources = MAX_RESOURCES;
-//	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-//	int increase_count_monitor(int);
-//	int decrease_count_monitor(int);
-//};
-//monitor resources {
-//	int available_resources;
-//	condition resources_avail;
-//	int decrease_count(int count)
-//	{
-//		while (available_resources < count)
-//			resources_avail.wait();
-//		available_resources -= count;
-//	}
-//	int increase_count(int count) {
-//		available_resources += count;
-//		resources_avail.signal();
-//	}
-//}
 
 int main(int argc, char *argv[]){
 	if (argc != 2){
@@ -43,7 +18,6 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 	int num_threads = atoi(argv[1]);
-	char letter;
 
 	int i = 0;
 	int resources_requested = 0;
@@ -69,30 +43,14 @@ int main(int argc, char *argv[]){
 
 }
 
-/**
- * Uses a mutex to stabilize the race condition
- * from part A.
- */
 void* partC(void *resources){
 
-	//while(1)
-	//{
-		pthread_mutex_lock(&mutex);
-//		if((int)available_resources<(int)resources)
-//		{ // that number of resources is not available, concede control of resource.
-//			pthread_mutex_unlock(&mutex);
-//		}
-		//else
-		//{
-			decrease_count((int)resources); // that number of resources are available, so we take them.
-			printf("Taking %d resources current count = %d\n",(int)resources,(int)available_resources);
-			//pthread_mutex_unlock(&mutex); // concede control
-			//break;
-	//}
-	//}
+	pthread_mutex_lock(&mutex);
+	decrease_count((int)resources); // that number of resources are available, so we take them.
+	printf("Taking %d resources current count = %d\n",(int)resources,(int)available_resources);
+
 
 	sleep(rand()%5);
-	//pthread_mutex_lock(&mutex); // lock control of available_resources
 	increase_count((int)resources); // give the resources back
 	printf("Returning %d resources current count = %d\n",(int)resources,(int)available_resources);
 	pthread_mutex_unlock(&mutex);
@@ -110,14 +68,8 @@ void decrease_count(int count)
 {
 	if (available_resources < count)
 	{
-		//return -1;
 		pthread_cond_wait(&cond, &mutex);
 	}
-	//else
-	//{
-		available_resources = available_resources - count;
-	//}
-//	while (available_resources < count)
-//		wait(mutex);
-//	available_resources -= count;
+	available_resources = available_resources - count;
+
 }
